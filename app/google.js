a418f28610b18cd19aea60374ec720ef0f7bb3ac
ghostwriter <ghostwriternr@gmail.com>
2016-03-24 07:01:51 +0530

    Move APIs to 'app' branch

---
+++ app/google.js
@@ -0,0 +1,15 @@
+// var http=require('http');
+// var request=require('request');
+
+// var test=function(){
+// 	console.log('here');
+// 	request.get({'url':'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL7mSAxuBjwt8MDfDOq6nswV8z7a5Yq5_I&key=AIzaSyD6CYoeesmOj6j91VPRvdngxFR7XqSTjgw','proxy':"http://10.3.100.207:8080"},function(error,response,body){
+// 		 console.log(body);
+// 		});
+// }
+
+// test();
+
+var express=require('express');
+var router=express.Router();
+var request=require('')
---
+++ app/routes/APIRouter.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+var Users=require('../models/Users');
+
+router.get('/',function(req,res){
+	res.send({'message':'You Made It!'});
+});
+
+router.use(function (req, res, next) {
+	console.log('herer');
+	if(req.query.token && req.query.fbid) {
+		var FB_URL = "https://graph.facebook.com/me?";
+		request.get(FB_URL + 'access_token=' + req.query.token, function (error, response, body) {
+			if(error) {
+				res.send({ status : 500, err : err});
+			}
+			var responseData = JSON.parse(body);
+			if(responseData.id != req.query.fbid) {
+				res.send({ status : 400, err : "Not Authorized"});
+			}
+			Users.count({ 'facebook.id' : responseData.id }, function(err, count) {
+				if(error) {
+					res.send({ status : 500, err : err}); return;
+				}
+				if(count == 1) {
+					next();
+					return;
+				} else {
+					var user = new Users();
+						user.fname = responseData.first_name;
+						user.lname = responseData.last_name;
+						user.facebook.id = responseData.id;
+
+					user.save(function (err) {
+						if(err) {
+							res.send({ status : 500, err : err}); return;
+						}
+						next();
+						return;
+					});
+				}
+			});
+		});
+	} else {
+		res.send({ status : 400, err : "No Access Token or User ID Passed"});
+	}
+});
+
+module.exports=router;
